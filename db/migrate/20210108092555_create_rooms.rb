b291f0d5f4f524605770e2fce2f6ae14807d6563
yamato1001 <yamato1001apa@i.softbank.jp>
2021-01-08 19:10:27 +0900

    room_usersテーブルの作成

--- db/migrate/20210108092555_create_rooms.rb
+++ db/migrate/20210108092555_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
       t.string :name, null: false
-      t.timestamps
+      t.timestamps   
     end
   end
 end
