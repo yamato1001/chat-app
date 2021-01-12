220bee2db9ad28371ad817671a89f9e7851c305c
yamato1001 <yamato1001apa@i.softbank.jp>
2021-01-12 12:42:34 +0900

    エラー修正

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,9 +3,10 @@ FactoryBot.define do
     content {Faker::Lorem.sentence}
     association :user
     association :room
-  end
+
 
   after(:build) do |message|
     message.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
+end
 end
